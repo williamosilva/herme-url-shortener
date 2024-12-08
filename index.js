622b29933280c5714f4c2b980236683d3401bde0
William Oliveira Silva <williamsilva20062005@gmail.com>
2024-12-08 14:31:28 -0300

    add MongoDB connection utility and integrate URL router in main application

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+const urlRouter = require("./routes/url");
 const app = express();
-
 const PORT = 8001;
+
+app.use("/url", urlRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
