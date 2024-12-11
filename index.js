532ce3a63d102b0f10526c4b410a1465bb9e4b97
William Silva <williamsilva20062005@gmail.com>
2024-12-11 12:22:30 -0300

    feat: add CORS headers and OPTIONS method handling to API key validation middleware

--- index.js
+++ index.js
@@ -38,8 +38,18 @@ app.get("/:shortId", async (req, res) => {
   }
 });
 
-// Middleware de validação da API Key
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-api-key"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
   const apiKey = req.headers["x-api-key"];
   if (!apiKey || apiKey !== API_KEY) {
     return res.status(403).json({ message: "Forbidden: Invalid API Key" });
